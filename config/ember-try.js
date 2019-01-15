97868e76730bed6ab5885e0320a42c3924fa7319
Katie Gengler <katie@kmg.io>
2019-01-15 16:14:08 -0500

    Use yarn for ember-try, comment out betas

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,5 +1,6 @@
 module.exports = function() {
   return {
+    useYarn: true,
     scenarios: [
       {
         name: 'ember-3.5',
@@ -9,38 +10,38 @@ module.exports = function() {
           }
         }
       },
-      {
-        name: 'ember-3.6-beta-1',
-        npm: {
-          devDependencies: {
-            'ember-source': '3.6.0-beta.1',
-          }
-        }
-      },
-      {
-        name: 'ember-3.6-beta-2',
-        npm: {
-          devDependencies: {
-            'ember-source': '3.6.0-beta.2',
-          }
-        }
-      },
-      {
-        name: 'ember-3.6-beta-3',
-        npm: {
-          devDependencies: {
-            'ember-source': '3.6.0-beta.3',
-          }
-        }
-      },
-      {
-        name: 'ember-3.6-beta-4',
-        npm: {
-          devDependencies: {
-            'ember-source': '3.6.0-beta.4',
-          }
-        }
-      },
+      // {
+      //   name: 'ember-3.6-beta-1',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': '3.6.0-beta.1',
+      //     }
+      //   }
+      // },
+      // {
+      //   name: 'ember-3.6-beta-2',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': '3.6.0-beta.2',
+      //     }
+      //   }
+      // },
+      // {
+      //   name: 'ember-3.6-beta-3',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': '3.6.0-beta.3',
+      //     }
+      //   }
+      // },
+      // {
+      //   name: 'ember-3.6-beta-4',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': '3.6.0-beta.4',
+      //     }
+      //   }
+      // },
       {
         name: 'ember-3.6',
         npm: {
